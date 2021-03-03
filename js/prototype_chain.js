2d0166a80042c5569a7fec82a552bedd772964cf
ixguza <rtk0253@gmail.com>
2021-03-03 15:41:11 +0900

    js Object.create

--- js/prototype_chain.js
+++ js/prototype_chain.js
@@ -29,6 +29,7 @@ p_not_new === undefined
 
 ////////////////////////////////////////
 // __proto__ and prototype
+// __proto__
 Array.__proto__ === Function.prototype
 String.__proto__ === Function.prototype
 (()=>{}).__proto__ === Function.prototype
@@ -41,21 +42,55 @@ String.__proto__ === Function.prototype
 p.__proto__ === Point.prototype
 p.__proto__.__proto__ === Object.__proto__
 
+// getPrototypeOf
+Object.getPrototypeOf(p) === p.__proto__
+
+// prototype
 [].prototype === undefined
 p.prototype === undefined
 (()=>{}).prototype === undefined
 
 Function.prototype instanceof Object
-
 Point.prototype instanceof Object
 Point.prototype.__proto__ === Object.prototype
 
+// constructor
+Point.prototype.constructor === Point
+Point.constructor === Function
+p.constructor === Point
+
+// Object.create
+// ppp ---> p ---> Point.prototype ---> Object.prototype
+const ppp = Object.create(p)
+ppp.__proto__ === p
+ppp.constructor === Point
+!(ppp.__proto__ === Point.prototype)
+
+p.hasOwnProperty('x') === true
+ppp.hasOwnProperty('x') === false
+
+const n = null
+try{ n.hasOwnProperty }catch(e){console.log(e)} // error
+/*
+TypeError: Cannot read property 'hasOwnProperty' of null
+    at <anonymous>:1:8
+*/
+
+const n2 = Object.create(null)
+n2.hasOwnProperty === undefined
+
+(n2.__proto__ === null) === false // not Object
+n2.__proto__ === undefined
+
+try{ var u = Object.create(undefined) }catch(e){console.log(e)} // error
+/*
+TypeError: Object prototype may only be an Object or null: undefined
+    at Function.create (<anonymous>)
+    at <anonymous>:1:21
+*/
 
 
 
-Point.__proto__ === Function.prototype
 
 
-// TODO getPrototypeOf
-//
-//
+
