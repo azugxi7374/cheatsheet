2656a06bff03fea53e72d18375e0ab5e132c280c
ixguza <rtk0253@gmail.com>
2021-03-29 20:07:03 +0900

    upd cli

---
+++ cli_tools.sh
@@ -0,0 +1,19 @@
+# git
+git config --global user.name <name>
+git config --global user.email <email>
+
+
+
+# vagrant
+vagrant init bento/ubuntu-18.04
+vagrant box add centos/8
+
+vagrant up
+vagrant ssh # vagrant/vagrant
+
+vagrant status
+vagrant halt
+vagrant reload
+
+vagrant global-status
+
--- cmds.sh
+++
@@ -1,3 +0,0 @@
-# git
-git config --global user.name <name>
-git config --global user.email <email>
