4595305cd30aad19a172a12b4d9c37b4ba0c40c0
azugxi7374 <3647920+azugxi7374@users.noreply.github.com>
2022-09-01 19:25:18 +0900

    033

--- html_css/tips_htmlcss.js
+++ html_css/tips_htmlcss.js
@@ -21,3 +21,7 @@ const [onload, loaded] = onloadWrapper
 const uri=""
 const encoded = encodeURI(uri)
 decodeURI(encoded)
+
+
+btoa("hogepiyo") // 'aG9nZXBpeW8='
+atob(btoa("hogepiyo")) // 'hogepiyo'
