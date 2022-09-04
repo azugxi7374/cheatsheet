da6aa669b4762b2ccfd18dae9ee5ca7ac7ce3d53
azugxi7374 <3647920+azugxi7374@users.noreply.github.com>
2022-09-04 22:24:46 +0900

    036

--- js/cheatsheet.js
+++ js/cheatsheet.js
@@ -6,6 +6,14 @@
     const [c, d] = [1, 2]
 }
 
+Array.from(document.querySelectorAll('div'))
+Array(1,2) // (2) [1, 2]
+Array(3) // (3) [empty × 3]
+Array.of(1) // [1]
+Array.of(1,2,3) // (3) [1, 2, 3]
+Object.fromEntries([["hoge",1],["piyo",2]]) // {hoge: 1, piyo: 2}
+
+
 const uri=""
 const encoded = encodeURI(uri)
 decodeURI(encoded)
