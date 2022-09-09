f3b0e1fdf60db509038b199f0cd3a210cb9d00fe
ixguza <rtk0253@gmail.com>
2022-09-09 23:41:47 +0900

    041

--- shell.sh
+++ shell.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
 find . -type f -name '*.pdf'
 
-for i in `seq 1 7`; do echo "i = $i"; done
+for i in $(seq 1 7); do echo "i = $i"; done
+
+for f in $(find . -type f -name '*.sh'); do echo "$f"; done
+# ↓空白がある場合はreadで読む
+find . -type f -name '*.sh' | while read arr; do echo "${arr[*]}"; done
 
 
+diff <(command1) <(command2)
+
+VAR=`ls`
+VAR=$(ls)
+
+
+
+
+exit 0
