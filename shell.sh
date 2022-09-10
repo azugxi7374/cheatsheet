4efd5e67e93af66fb5bf3de35ce6d2683bec4513
ixguza <rtk0253@gmail.com>
2022-09-10 22:23:36 +0900

    042

--- shell.sh
+++ shell.sh
@@ -13,8 +13,34 @@ diff <(command1) <(command2)
 
 VAR=`ls`
 VAR=$(ls)
+VAR=`ls hogehoge` # ""
+VAR=`ls hogehoge 2>&1 >/dev/null` # ls: hogehoge: No such file or directory
+
+unset VAR
 
 
+echo $? # 終了ステータス
+echo $! # バックグラウンドプロセス
+echo $$ # プロセス
+echo $# # 引数の数
+echo $0
+echo $1 $2 # ...
+
+Hoge() {
+    echo '$*'
+    for s in $*; do echo $s; done
+    echo '"$*"'
+    for s in "$*"; do echo $s; done
+}
+Hoge aaa bbb ccc
+<<HEREDOC
+$*
+aaa
+bbb
+ccc
+"$*"
+aaa bbb ccc
+HEREDOC
 
 
 exit 0
