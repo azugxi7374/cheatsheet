b48c43f5a314d417f4ff50b7f23acacdb892792b
ixguza <rtk0253@gmail.com>
2022-09-12 20:31:11 +0900

    044

--- html_css/tips_htmlcss.js
+++ html_css/tips_htmlcss.js
@@ -1,6 +1,6 @@
 // CSSの解決値を取得
 // getComputedStyle
-const e = {/*...*/}; // 要素
+const e = {/*...*/ }; // 要素
 const compStyles = document.defaultView.getComputedStyle(e)
 compStyles.width
 compStyles.color
@@ -18,10 +18,21 @@ const [onload, loaded] = onloadWrapper
 // <... onload="onload()" ... />
 // loaded.then(()=>...)
 
-const uri=""
+const uri = ""
 const encoded = encodeURI(uri)
 decodeURI(encoded)
 
 
 btoa("hogepiyo") // 'aG9nZXBpeW8='
 atob(btoa("hogepiyo")) // 'hogepiyo'
+
+
+
+/////////////////////
+// HTMLのサイズ関連
+// スマホで画面サイズ取得に使う
+[window.parent.screen.height, window.parent.screen.width] // window.topでいいのでは？
+const elem1 = document.body;
+[elem1.clientHeight, elem1.clientWidth] // padding含む内側サイズ
+const elem2 = document.querySelector('a')
+[elem2.clientHeight, elem2.clientWidth] // inlineでは0
