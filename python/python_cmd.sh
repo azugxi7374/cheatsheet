42fcf353b8c154d5b7740327399942585c8abd02
ixguza <rtk0253@gmail.com>
2023-05-16 14:07:26 +0900

    add pipenv

--- python/python_cmd.sh
+++ python/python_cmd.sh
@@ -3,3 +3,13 @@ python -m "http.server" 8888
 
 python -c 'print("hoge")'
 echo 'print("hoge")' | python -
+
+# pipenv
+pipenv
+pipenv graph
+pipenv install
+pipenv install --skip-lock
+pipenv shell
+pipenv --clear
+
+
