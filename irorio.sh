be7d1380733be4642c475eb458d4a6a38a1de8ae
ixguza <rtk0253@gmail.com>
2023-05-17 14:22:47 +0900

    gh

--- irorio.sh
+++ irorio.sh
@@ -11,3 +11,7 @@ _text="ずんだもんなのだ"
 _host="localhost:50021"
 _text_encoded=$(node -e 'console.log(encodeURI(`'$_text'`))'); _temp=`mktemp`; curl -H 'accept: application/json' -XPOST "$_host/audio_query?text=$_text_encoded&speaker=$_speaker" | tee query.json | curl -H 'Accept: audio/wav' -H 'Content-Type: application/json' -XPOST "$_host/synthesis?speaker=$_speaker&enable_interrogative_upspeak=true" -d @- > $_temp; afplay $_temp; rm -f $_temp
 
+
+########################
+# github
+gh gist create gistfilename -f file.txt
