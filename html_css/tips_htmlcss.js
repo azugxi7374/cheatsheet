d87adfbc515ec83ac330af52ff3f938dd7571acc
ixguza <rtk0253@gmail.com>
2023-06-14 23:51:49 +0900

    add js speech api

--- html_css/tips_htmlcss.js
+++ html_css/tips_htmlcss.js
@@ -58,3 +58,13 @@ document.addEventListener('DOMContentLoaded', () => { });
     'input'
 
 }
+
+// speech
+function speechCancel() {
+    window.speechSynthesis.cancel();
+}
+function speech(text) {
+    synthesis = window.speechSynthesis
+    utterance = new SpeechSynthesisUtterance(text);
+    synthesis.speak(utterance);
+}
