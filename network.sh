51e46b9e09d22b5e3eebc712f707844a019ce41e
ixguza <rtk0253@gmail.com>
2024-08-23 17:47:10 +0900

    upd network

--- network.sh
+++ network.sh
@@ -17,6 +17,7 @@ dig google.com @8.8.8.8
 telnet www.google.co.jp 80
 { printf 'HEAD / HTTP/1.1\nHost: www.google.co.jp\n\n'; sleep 3;} | telnet www.google.co.jp 80
 
+arp -a
 
 # global ip
 curl inet-ip.info
